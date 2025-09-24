310e4eaeff4afafe40b9d8a2b30fb817590a3fb5
Vraxh <barrosownicolas@gmail.com>
2025-09-24 12:31:13 -0300

    Agrega funtion getHumanchoice
    Agrega logica basica en la funcion para que el usuario decida

--- rpsGame.js
+++ rpsGame.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
 
     let numRan = Math.random() * (4 - 1) + 1;
-    let rps = "vacio";
-    
+    let rps = "Rock_Paper_Scisors";
+
     if(numRan >= 1 && numRan < 2){
         rps = "Rock";
     }else if (numRan >= 2 && numRan < 3){
@@ -13,4 +13,20 @@ function getComputerChoice() {
     return rps;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(choice){
+
+    let rps = "Rock_Paper_Scisors";
+    if(choice == 1){
+        rps = "Rock";
+    }else if (choice == 2){
+        rps = "Paper";
+    }else if (choice == 3){
+        rps = "Scisors";
+    }
+    return rps;
+}
+
+
+result = window.prompt("1 - Rock | 2 - Paper | 3 - Scisors");
+console.log(getHumanChoice(result));
+
