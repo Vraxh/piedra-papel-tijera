458ced83cb3346a53074f7ecd5d5e9675be01f35
Vraxh <barrosownicolas@gmail.com>
2025-09-24 11:52:39 -0300

    Agrega funtion "getComputerChoice"
    Agrega funtion logica copleta para elegir uno de los elementos

--- rpsGame.js
+++ rpsGame.js
@@ -1,1 +1,16 @@
-console.log("Hola");
+function getComputerChoice() {
+
+    let numRan = Math.random() * (4 - 1) + 1;
+    let rps = "vacio";
+    
+    if(numRan >= 1 && numRan < 2){
+        rps = "Rock";
+    }else if (numRan >= 2 && numRan < 3){
+        rps = "Paper";
+    }else{
+        rps = "Scisors";
+    }
+    return rps;
+}
+
+console.log(getComputerChoice());
